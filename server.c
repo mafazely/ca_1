ac3b048218a27055ea725aa17b3855364dd87ae0
mafazely <aminfazeli00@gmail.com>
2020-10-22 14:37:11 +0330

    server.c created

---
+++ server.c
@@ -0,0 +1,22 @@
+#include <arpa/inet.h>
+#include <errno.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <signal.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/time.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+#define TRUE 1
+#define FALSE 0
+
+#define MAXSIZE 1024 /*max buffer size*/
+#define SERV_PORT 8080 /*port*/
+#define LISTENQ 8 /*maximum number of client connections*/
+#define MAX_CLIENTS_COUNT 20
+
