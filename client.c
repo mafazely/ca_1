b511ed7aaa2e881ab65f9a325f3b051ab178293c
mafazely <aminfazeli00@gmail.com>
2020-10-22 18:17:11 +0330

    add main and connection implementation

--- client.c
+++ client.c
@@ -43,4 +43,54 @@ void Create_TCP_Connection()
     {
         perror("Error : Connect Failed !");
     }
+    else
+        perror("connected to the server...");
+}
+
+int main(int argc, char const *argv[])
+{
+    char buffer[MAXSIZE+1];
+    char *message = "Hello Server. It's me(client)!";
+    int valread;
+    int n, len;
+
+    Create_TCP_Connection();
+
+    memset(buffer, 0, sizeof(buffer));
+    strcpy(buffer, message);
+    write(client_fd, buffer, sizeof(buffer));
+
+
+    //printf("Message from server: ");
+    if ((valread = read(client_fd, buffer, 1024)) == 0)
+    {
+        /* Somebody disconnected , get his details and print */
+        getpeername(client_fd, (struct sockaddr *)&servaddr, (socklen_t *)&servaddr);
+        printf("Host disconnected , ip %s , port %d \n",
+               inet_ntoa(servaddr.sin_addr), ntohs(servaddr.sin_port));
+
+        /* Close the socket and mark as 0 in list for reuse */
+        //close(client_fd);
+    }
+
+    /* Echo back the message that came in */
+    else
+    {
+        /* set the string terminating NULL byte on the end
+         * of the data read */
+        buffer[valread] = '\0';
+        printf("the receievd message is: %s\n", buffer);
+
+        if (send(client_fd, message, strlen(message), 0) < 0)
+        {
+            perror("sending message failed");
+        }
+        else
+            printf("the sent message : %s \n", message);
+    }
+
+    // read(client_fd, buffer, sizeof(buffer));
+    // puts(buffer);
+
+    return 0;
 }
